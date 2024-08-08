4b5b66c455f6e7dc33e0ae1a6211988b15e167d6
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-08-09 01:40:35 +0300

    Add files via upload

---
+++ frontend/src/utils/translations.js
@@ -0,0 +1,300 @@
+const translations = {
+  en: {
+    confirmOrder: 'Confirm Order',
+    name: 'Name',
+    enterName: 'Enter your name',
+    phone: 'Phone',
+    enterPhone: 'Enter your phone number',
+    address: 'Address',
+    enterAddress: 'Enter your address',
+    delivery: 'Delivery',
+    deliveryFee: 'Delivery Fee',
+    enterDeliveryFee: 'Enter delivery fee',
+    submitOrder: 'Submit Order',
+    currency: 'K.D',
+    addAddressModal: {
+      addNewAddress: "Add New Address",
+      country: "Country",
+      region: "City",
+      road: "Road",
+      kuwait: "Kuwait (KW)",
+      area: "Area",
+      selectArea: "Select Area",
+      home: "Home",
+      apartment: "Apartment",
+      office: "Office",
+      block: "Block",
+      enterBlock: "Enter Block",
+      street: "Street",
+      enterStreet: "Enter Street",
+      house: "House",
+      enterHouse: "Enter House",
+      avenue: "Avenue",
+      enterAvenue: "Enter Avenue",
+      specialDirections: "Special Directions",
+      enterDirections: "Enter Directions",
+      makeDefault: "Make Default",
+      close: "Close",
+      add: "Add",
+      optional: "Optional",
+    },
+    citySelectModal: {
+      selectCity: "Select City",
+      close: "Close"
+    },
+    countrySelectModal: {
+      selectCountry: "Select Country",
+      close: "Close"
+    },
+    cart: {
+      reviewOrder: 'Review Order',
+      yourCartIsEmpty: 'Your cart is empty',
+      startShopping: 'Start shopping and choose the right product',
+      startShoppingButton: 'Start Shopping',
+      subtotal: 'Subtotal',
+      deliveryFee: 'Delivery Fee',
+      total: 'Total',
+      proceedToCheckout: 'Proceed to Checkout'
+    },
+    footer: {
+      more: 'More',
+      myAccount: 'My Account',
+      login: 'Login',
+      cart: 'Cart',
+      orderStatus: 'Order Today',
+      orderStatusAll: 'Order all',
+      menu: 'Menu'
+    },
+    accountModal: {
+      myAccount: 'My Account',
+      updateProfile: 'Update Profile',
+      addresses: 'Addresses',
+      changePassword: 'Change Password',
+      logout: 'Logout'
+    },
+    addressesModal: {
+      deliveryAddress: 'Delivery Address',
+      addressEmpty: 'Address list is empty',
+      addAddress: 'Add New Address',
+      deleteConfirmation: 'Delete Confirmation',
+      confirmDelete: 'Are you sure you want to delete this address?',
+      cancel: 'Cancel',
+      delete: 'Delete',
+      default: 'Default',
+    },
+    changePasswordModal: {
+      changePassword: 'Change Password',
+      oldPassword: 'Old Password',
+      enterOldPassword: 'Enter old password',
+      newPassword: 'New Password',
+      enterNewPassword: 'Enter new password',
+      confirmNewPassword: 'Confirm New Password',
+      confirmNewPasswordPlaceholder: 'Confirm new password',
+      confirm: 'Confirm'
+    },
+    customModal: {
+      moreOptions: 'More Options',
+      contactUs: 'Contact Us',
+      aboutUs: 'About Us',
+      privacyPolicy: 'Privacy Policy',
+      arabic: 'Arabic',
+      english: 'English'
+    },
+    loginModal: {
+      login: 'Login',
+      email: 'Email',
+      enterEmail: 'Enter your email',
+      password: 'Password',
+      enterPassword: 'Enter your password',
+      forgotPassword: 'Forgot Password?',
+      dontHaveAccount: "Don't have an account?",
+      signUp: 'Sign up'
+    },
+    moreModal: {
+      moreOptions: 'More Options',
+      contactUs: 'Contact Us',
+      aboutUs: 'About Us',
+      privacyPolicy: 'Privacy Policy',
+      close: 'Close'
+    },
+    signUpModal: {
+      createAccount: 'Create Account',
+      country: 'Country',
+      selectCountry: 'Select your country',
+      kuwait: 'Kuwait',
+      phone: 'Phone Number',
+      enterPhone: 'Enter your phone number',
+      name: 'Name',
+      enterName: 'Enter your name',
+      email: 'Email',
+      enterEmail: 'Enter your email',
+      password: 'Password',
+      enterPassword: 'Enter your password',
+      confirm: 'Confirm',
+      terms: 'By signing up, you agree to our',
+      termsConditions: 'Terms & Conditions'
+    },
+    updateProfileModal: {
+      updateProfile: 'Update Profile',
+      name: 'Name',
+      email: 'Email',
+      country: 'Country',
+      phone: 'Phone Number',
+      update: 'Update'
+    },
+    categorySections: {
+      addToCart: 'Add to Cart'
+    }
+  },
+  ar: {
+    confirmOrder: 'تأكيد الطلب',
+    name: 'الاسم',
+    enterName: 'أدخل اسمك',
+    phone: 'رقم الهاتف',
+    enterPhone: 'أدخل رقم هاتفك',
+    address: 'العنوان',
+    enterAddress: 'أدخل عنوانك',
+    delivery: 'التوصيل',
+    deliveryFee: 'رسوم التوصيل',
+    enterDeliveryFee: 'أدخل رسوم التوصيل',
+    submitOrder: 'أرسل الطلب',
+    currency: 'د.ك',
+    addAddressModal: {
+      addNewAddress: "إضافة عنوان جديد",
+      country: "البلد",
+      region: "المدينة",
+      road: "الطريق",
+      specialDirections: "إرشادات خاصة",
+      selectCountry: "اختر البلد",
+      kuwait: "الكويت",
+      region: "المنطقة",
+      selectRegion: "اختر المنطقة",
+      home: "منزل",
+      apartment: "شقة",
+      office: "مكتب",
+      block: "القطعة",
+      enterBlock: "أدخل القطعة",
+      street: "الشارع",
+      enterStreet: "أدخل الشارع",
+      house: "المنزل",
+      enterHouse: "أدخل المنزل",
+      avenue: "الجادة",
+      enterAvenue: "أدخل الجادة",
+      specialDirections: "إرشادات خاصة",
+      enterDirections: "أدخل الإرشادات",
+      makeDefault: "اجعلها افتراضية",
+      close: "إغلاق",
+      add: "إضافة",
+      optional: "اختياري",
+    },
+    citySelectModal: {
+      selectCity: "اختر المدينة",
+      close: "إغلاق"
+    },
+    countrySelectModal: {
+      selectCountry: "اختر البلد",
+      close: "إغلاق"
+    },
+    cart: {
+      reviewOrder: 'قم بمراجعة الطلب',
+      yourCartIsEmpty: 'سلة التسوق الخاصة بك فارغة',
+      startShopping: 'ابدأ التسوق واختر المنتج المناسب',
+      startShoppingButton: 'ابدأ التسوق',
+      subtotal: 'المجموع الفرعي',
+      deliveryFee: 'رسوم التوصيل',
+      total: 'المجموع',
+      proceedToCheckout: 'الذهاب لإتمام الطلب'
+    },
+    footer: {
+      more: 'المزيد',
+      myAccount: 'حسابي',
+      login: 'تسجيل الدخول',
+      cart: 'السلة',
+      orderStatus: 'طلبات اليوم',
+      orderStatusAll: 'جميع الطلبات',
+      menu: 'القائمة'
+    },
+    accountModal: {
+      myAccount: 'حسابي',
+      updateProfile: 'تحديث الملف الشخصي',
+      addresses: 'العناوين',
+      changePassword: 'تغيير كلمة المرور',
+      logout: 'تسجيل خروج'
+    },
+    addressesModal: {
+      deliveryAddress: 'عنوان التسليم',
+      addressEmpty: 'قائمة العناوين فارغة',
+      addAddress: 'إضافة عنوان جديد',
+      deleteConfirmation: 'تأكيد الحذف',
+      confirmDelete: 'هل أنت متأكد أنك تريد حذف هذا العنوان؟',
+      cancel: 'إلغاء',
+      delete: 'حذف',
+      default: 'افتراضي',
+    },
+    changePasswordModal: {
+      changePassword: 'تغيير كلمة المرور',
+      oldPassword: 'كلمة المرور القديمة',
+      enterOldPassword: 'أدخل كلمة المرور القديمة',
+      newPassword: 'كلمة المرور الجديدة',
+      enterNewPassword: 'أدخل كلمة المرور الجديدة',
+      confirmNewPassword: 'تأكيد كلمة المرور الجديدة',
+      confirmNewPasswordPlaceholder: 'أدخل تأكيد كلمة المرور الجديدة',
+      confirm: 'تأكيد'
+    },
+    customModal: {
+      moreOptions: 'المزيد',
+      contactUs: 'إتصل بنا',
+      aboutUs: 'من نحن',
+      privacyPolicy: 'سياسة الخصوصية',
+      arabic: 'العربية',
+      english: 'الإنجليزية'
+    },
+    loginModal: {
+      login: 'تسجيل الدخول',
+      email: 'البريد الإلكتروني',
+      enterEmail: 'أدخل بريدك الإلكتروني',
+      password: 'كلمة المرور',
+      enterPassword: 'أدخل كلمة المرور',
+      forgotPassword: 'نسيت كلمة المرور؟',
+      dontHaveAccount: 'لا تملك حساب؟',
+      signUp: 'تسجيل'
+    },
+    moreModal: {
+      moreOptions: 'خيارات أكثر',
+      contactUs: 'إتصل بنا',
+      aboutUs: 'من نحن',
+      privacyPolicy: 'سياسة الخصوصية',
+      close: 'إغلاق'
+    },
+    signUpModal: {
+      createAccount: 'إنشاء حساب',
+      country: 'البلد',
+      selectCountry: 'اختر بلدك',
+      kuwait: 'الكويت',
+      phone: 'رقم الهاتف',
+      enterPhone: 'أدخل رقم هاتفك',
+      name: 'الاسم',
+      enterName: 'أدخل اسمك',
+      email: 'البريد الإلكتروني',
+      enterEmail: 'أدخل بريدك الإلكتروني',
+      password: 'كلمة المرور',
+      enterPassword: 'أدخل كلمة المرور',
+      confirm: 'تأكيد',
+      terms: 'بالتسجيل، أنت توافق على',
+      termsConditions: 'الشروط والأحكام'
+    },
+    updateProfileModal: {
+      updateProfile: 'تحديث الملف الشخصي',
+      name: 'الاسم',
+      email: 'البريد الإلكتروني',
+      country: 'البلد',
+      phone: 'رقم الهاتف',
+      update: 'تحديث'
+    },
+    categorySections: {
+      addToCart: 'أضف إلى السلة'
+    }
+  }
+};
+
+export default translations;
