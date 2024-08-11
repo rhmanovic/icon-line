c4d0e80b73b8b9d021a2da2e0c8a83703ef31aad
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-08-11 22:32:02 +0300

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, 'build')));
 
-// The "catchall" handler: for any request that doesn't match one above, send back the index.html file from the main directory.
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+console.log(`Server is running on port ${PORT}`);
 });
