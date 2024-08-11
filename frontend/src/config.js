7c7c1fba92ccbe8d80adac388c646dd38dea8c75
rhmanovic <31311273+rhmanovic@users.noreply.github.com>
2024-08-12 00:23:18 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 // config.js
-// export const BASE_URL = 'https://arabatcms.replit.app';
-export const BASE_URL = 'https://0d1631b7-2ce3-431e-af36-c7e4fc0f0954-00-1sp5nem2n8wmo.sisko.replit.dev';
+export const BASE_URL = 'https://arabatcms.replit.app';
+// export const BASE_URL = 'https://0d1631b7-2ce3-431e-af36-c7e4fc0f0954-00-1sp5nem2n8wmo.sisko.replit.dev';
 export const YOUR_MERCHANT_ID = '66a02ec7941f4eca406c7707';
