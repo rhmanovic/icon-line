0b3ac1912517915a96baaa7b02c69246c456c8a0
rhmanovic <eng.dugaim@gmail.com>
2024-08-11 19:41:14 +0000

    Commit message describing the changes

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-// vite.congfig.js
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -16,4 +13,10 @@ export default defineConfig({
       },
     },
   },
-})
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/index.html',
+    },
+  },
+});
